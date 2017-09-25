b43f92d06ff7c4a0d4d9fe7c5dc8f6bcd087a59f
jyorkcar81 <1dog1cat>
2017-09-25 16:07:18 -0700

    test message

---
+++ app/src/main/java/com/saccity362/firstgithubapp/FirstGithubActivity.java
@@ -0,0 +1,13 @@
+package com.saccity362.firstgithubapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstGithubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_github);
+    }
+}
